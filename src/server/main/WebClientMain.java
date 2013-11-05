5e59676137894edce1e61e0d6ad487003f6e4980
alo <alokhan_1@hotmail.com>
2013-11-05 13:25:25 +0100

    added the possibility to ask multiple url at the same time moved every request into a thread

--- src/server/main/WebClientMain.java
+++ src/server/main/WebClientMain.java
@@ -1,35 +1,21 @@
 package server.main;
 
-import server.http.Request;
-import server.http.Response;
-import server.web.WebClient;
-
 import java.io.IOException;
-import java.net.URL;
+import server.web.WebClientThread;
 
 public class WebClientMain {
 
     public static void main(String[] args) throws IOException {
-        URL url = new URL(args[0]);
-        String header =
-                "Host:" + url.getHost() + "\r\n" +
-                        "Connection: keep-alive\r\n";
-        Request request = new Request("GET", args[0], "HTTP/1.1", header);
-        WebClient webClient = new WebClient();
-        Response resultFromRequest = webClient.SendRequest(request);
-        System.out.println(resultFromRequest.StatusLineAndHeaders());
-        System.out.println("--------- Content ------------");
-        byte[] arr = resultFromRequest.getContent().getBytes("iso-8859-1");
-        // Create a new String using the contents of the byte array.
-        String newStr = new String(arr);
-
-        System.out.println(newStr);
-        System.out.println(newStr.length());
-//        OutputStream out;
-//        FileOutputStream stream = new FileOutputStream("test.html");
-//        out = new BufferedOutputStream(stream);
-//        out.write(arr);
-//        out.close();
+        
+        if (args.length < 1) {
+            System.out.println("Give at least one URL as an argument.");
+            return;
+        }
+        // for every given argument start a new thread
+        for (String urlString : args) {
+            Runnable r = new WebClientThread(urlString);
+            new Thread(r).start();   
+        }
     }
 
 }
---
+++ src/server/web/WebClientThread.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server.web;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import server.http.Request;
+import server.http.Response;
+
+/**
+ *
+ * @author alo
+ */
+public class WebClientThread implements Runnable {
+
+    private String urlString;
+
+    public WebClientThread(String url) {
+        urlString = url;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // trying to build a argument from the string argument 
+            URL url = new URL(urlString);
+            String header = "Host:" + url.getHost() + "\r\n"
+                    + "Connection: keep-alive\r\n";
+
+            // build a GET HTTP request
+            Request request = new Request("GET", url.toString(), "HTTP/1.1", header);
+
+            // sending the request to the web server
+            WebClient webClient = new WebClient();
+            Response resultFromRequest = webClient.SendRequest(request);
+
+            // Display the result
+            System.out.println("--------- Header ------------");
+            System.out.println(resultFromRequest.StatusLineAndHeaders());
+            System.out.println("--------- Content ------------");
+            System.out.println(resultFromRequest.getContent());
+            System.out.println(resultFromRequest.getContent().length());
+
+            OutputStream out;
+            FileOutputStream stream = new FileOutputStream(url.getHost() + url.getPath());
+            out = new BufferedOutputStream(stream);
+            out.write(resultFromRequest.getContent().getBytes());
+            out.close();
+            
+        } catch (MalformedURLException ex) {
+            System.out.println(urlString + "is a malformedURL");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
